ab94f3aefd4657d5eff4575c6257532941e7b5cb
Tredoggins <th5416.school@gmail.com>
2021-11-24 15:27:19 -0600

    Card stuff

--- src/components/CardViewer.vue
+++ src/components/CardViewer.vue
@@ -1,16 +1,29 @@
 <template>
-  <v-container>
+  <v-container class="b-1">
     <v-row>
       <v-col>
-        <v-select :items="colorItems" v-if="colors" label="Color" :value="colorItems[0]"></v-select>
+        <v-select :items="colorItems" v-if="colors" label="Color" v-model="selectedColor"></v-select>
       </v-col>
     </v-row>
-    <v-row>
-      <v-col v-for="card in cards" :key="card.id">
-        <v-card width="200">
-          <v-img :src="card.image_uris.large" v-if="card.image_uris"></v-img>
-          <h1 v-else>{{card.name}}</h1>
-        </v-card>
+    <v-row class="d-flex justify-space-between">
+      <v-col cols="1" class="d-flex align-center">
+        <v-btn fab @click="previous"><v-icon>mdi-arrow-left</v-icon></v-btn>
+      </v-col>
+      <v-col>
+        <v-container>
+          <v-row>
+            <v-col v-for="card in currentCards" :key="card.id" cols="2">
+              <v-card height="200" @click="clickCard(card)">
+                <v-img :src="card.image_uris.large" v-if="card.image_uris"></v-img>
+                <h1 v-else>{{card.name}}</h1>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+
+      </v-col>
+      <v-col cols="1" class="d-flex align-center">
+        <v-btn fab @click="next"><v-icon>mdi-arrow-right</v-icon></v-btn>
       </v-col>
     </v-row>
   </v-container>
@@ -26,23 +39,63 @@ export default {
     cards:{
       type:Array,
       required:true,
+    },
+    getMore:{
+      type:Function
     }
   },
   data:()=>({
+    selectedColor:{},
+    currentCards:[],
+    page:1,
   }),
+  methods:{
+    clickCard(card){
+      this.$emit('clickedCard',card);
+      this.setCurrentCards();
+    },
+    next(){
+
+    },
+    previous(){
+
+    },
+    setCurrentCards(){
+      if(((this.page-1)*6)<(this.cards.length)&&(this.page*6)<=(this.cards.length)) {
+        this.currentCards = this.cards.slice((this.page - 1) * 6, this.page * 6);
+      }
+      else if(((this.page-1)*6)<(this.cards.length)){
+        this.currentCards=this.cards.slice(((this.page-1)*6),(this.cards.length));
+      }
+      else{
+        this.currentCards=this.cards.slice(((this.cards.length-6)>=0?(this.cards.length-6):0),(this.cards.length))
+      }
+    }
+  },
   computed:{
     colorItems(){
       let newColors=[];
       newColors.push({text:"All",value:'all'})
       this.colors.forEach((val)=>{
-        newColors.push({text:val,value:val.toLowerCase()})
+        newColors.push({text:val,value:(val.toUpperCase()!=='BLUE'?val.toUpperCase().substr(0,1):'U')})
       });
       return newColors;
     },
+  },
+  mounted() {
+    this.selectedColor=this.colorItems[0];
+    this.setCurrentCards();
+  },
+  watch:{
+    cards:function (){
+      this.setCurrentCards();
+    }
   }
 }
 </script>
 
 <style scoped>
-
+ div.row.d-flex{
+   height: 300px;
+ }
 </style>
--- src/views/BuildDeck.vue
+++ src/views/BuildDeck.vue
@@ -9,14 +9,15 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col>
+      <v-col v-if="!loading">
         <v-text-field append-icon="mdi-magnify" label="search" v-model="search" @click:append="getCards"></v-text-field>
-        <card-viewer :cards="allCards" :colors="colors"></card-viewer>
+        <card-viewer :cards="allCards" :colors="colors" @clickedCard="addCardToDeck"></card-viewer>
       </v-col>
+      <v-progress-circular indeterminate v-else></v-progress-circular>
     </v-row>
     <v-row>
       <v-col>
-        <card-viewer :cards="deckCards" :colors="colors"></card-viewer>
+        <card-viewer :cards="deckCards" :colors="colors" @clickedCard="removeCardFromDeck"></card-viewer>
       </v-col>
     </v-row>
   </v-container>
@@ -26,6 +27,7 @@
 import CardViewer from "@/components/CardViewer";
 import {db} from "@/config/config";
 import axios from "axios";
+import firebase from "firebase";
 export default {
   name: "BuildDeck",
   props:{
@@ -44,6 +46,7 @@ export default {
     deck:{},
     colors:["black",'blue'],
     search:"",
+    loading:true,
   }),
   firestore(){
     return {
@@ -53,20 +56,37 @@ export default {
   },
   methods:{
     getCards(){
+      this.loading=true
       axios
           .get('https://api.scryfall.com/cards/search?q='+(this.search.length>0?this.search:"*"))
           .then((r)=>{
             this.allCards=r.data.data;
+            this.loading=false;
           })
           .catch((e)=>{
             console.error(e);
           })
+    },
+    addCardToDeck(card){
+      console.log(db.collection("users").doc(this.user.uid).collection("decks").doc(this.id).collection("cards").doc(card.id).get().then((r)=>{if(r.exists){
+          db.collection("users").doc(this.user.uid).collection("decks").doc(this.id).collection("cards").doc(card.id).update({qty:firebase.firestore.FieldValue.increment(1)}).then(()=>{}).catch((e)=>{console.error(e)})
+        }
+        else{
+          card.qty=1;
+          db.collection("users").doc(this.user.uid).collection("decks").doc(this.id).collection("cards").doc(card.id).set(card).then(()=>{}).catch((e)=>{console.error(e)})
+        }
+      }));
+
+    },
+    removeCardFromDeck(card){
+      //console.log(card);
+      db.collection("users").doc(this.user.uid).collection("decks").doc(this.id).collection("cards").doc(card.id).delete().then(()=>{}).catch((e)=>{console.error(e)})
     }
   },
   components:{
     CardViewer,
   },
-  mounted() {
+  created() {
     this.getCards();
   }
 }
